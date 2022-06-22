984610a487b373c378b961a9a027fd162ddb90b9
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2022-06-22 20:06:08 -0300

    Putting theme and a codeNode on editor

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LexicalLibStudy
  Repository to test and learn to use lexical lib of facebook
+
+## Construindo code lexical editor
+Para usar os plugins de code highlite precisa registrar os nodeCodeHighlightNode, CodeNode
--- lexical-code/src/App.tsx
+++ lexical-code/src/App.tsx
@@ -1,15 +1,23 @@
 import {LexicalComposer} from '@lexical/react/LexicalComposer'
 import {PlainTextPlugin} from '@lexical/react/LexicalPlainTextPlugin';
 import {ContentEditable} from '@lexical/react/LexicalContentEditable';
+import CodeHighlightPlugin from './Plugins/CodeHighlightPlugin';
+import {CodeHighlightNode, CodeNode} from '@lexical/code';
+import { $createParagraphNode, $createTextNode, $getRoot } from 'lexical';
+import {$createCodeNode, $createCodeHighlightNode} from '@lexical/code';
+
 
 import StyledEditor from "./components/StyledEditor"
+import playgroundTheme from './playgroundTheme';
 
 const initialConfig = {
   namespace: 'code-editor',
-  theme: {},
+  editorState: prePopulateEditor,
+  theme: playgroundTheme,
   onError(e: any) {
     console.log(e)
-  }
+  },
+  nodes: [CodeHighlightNode, CodeNode]
 }
 
 function App() {
@@ -20,6 +28,7 @@ function App() {
         <LexicalComposer initialConfig={initialConfig}>
           <PlainTextPlugin contentEditable={<ContentEditable />}
             placeholder=''/>
+          <CodeHighlightPlugin />
         </LexicalComposer>
       </StyledEditor>
     </div>
@@ -27,3 +36,14 @@ function App() {
 }
 
 export default App
+
+
+function prePopulateEditor() {
+  console.log('Teste')
+  const root = $getRoot()
+  // const paragraph = $createParagraphNode();
+  // const text = $createTextNode('Populando o node com text node') 
+  const codeNode = $createCodeNode('javascript')
+  codeNode.append($createCodeHighlightNode('const x = 2'))
+  root.append(codeNode)
+}
